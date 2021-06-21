d8c367d162d76b7b80de91a516f3b7b3c74b0110
Hieu <dthieu223@gmail.com>
2021-06-21 23:18:08 +0700

    Add express app

--- test.js
+++ test.js
@@ -15,13 +15,15 @@ const sequelize = new Sequelize(DB_CONFIG.DATABASE, DB_CONFIG.USERNAME, DB_CONFI
     language: 'en'
 })
 
-async function test() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-}
 
-test()
+var express = require('express')
+
+var app = express()
+
+app.get('/contact', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(3000, () => {
+    console.log('Listen on port 3000')
+})
