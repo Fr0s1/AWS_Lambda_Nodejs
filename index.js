6693f36064bf563bafd71efa386e550f83dc42fa
Hieu <dthieu223@gmail.com>
2021-06-23 10:31:45 +0700

    Add GET and DELETE operation: - GET: get all capitals or get capital by country name - DELTE: delete capital by country name

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ db.sequelize.sync();
 const countryCapital = db.countryCapital
 
 exports.handler = async (event, context) => {
+    
+    let httpMethod = event.context.http_method
+    switch (httpMethod) {
+        case 'GET':
+            if (event.params.querystring.name) {
+                return await getCapitalByCountryName(event.params.querystring.name)
+            } else {
+                return await getAllCapital()
+            }
+
+        case 'POST':
+            return await insertNew(event.body_json)
+
+        case 'DELETE':
+            return await deleteRecordByCountryName(event.body_json.name)
+    }
+    // return event
+
+}
+
+async function insertNew(event) {
     let result = await countryCapital.findAll({
         where: {
             name: event.name
@@ -18,5 +39,39 @@ exports.handler = async (event, context) => {
     }
     return {
         message: 'Already exists'
+    }
+}
+
+async function getAllCapital() {
+    let result = await countryCapital.findAll()
+
+    return result
+}
+
+async function getCapitalByCountryName(name) {
+    let result = await countryCapital.findAll({
+        where: {
+            name: name
+        }
+    })
+
+    return result
+}
+
+async function deleteRecordByCountryName(name) {
+    let result = await countryCapital.destroy({
+        where: {
+            name: name
+        }
+    })
+
+    if (result == 1) {
+        return {
+            message: `Deleted record with country name ${name}`
+        }
+    } else {
+        return {
+            message: `Record with country name ${name} doesn't exist.`
+        }
     }
 }
